3a65a814185206406050db93c6d58fc44761d4e4
Krzysiek <szlauerdev@gmail.com>
2025-02-16 13:00:25 +0100

    Building forms: 7

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,21 @@
-import { Alert } from './components/Alert';
-import { Button } from './components/Button/Button';
-import { LikeIcon } from './components/LikeIcon';
 import { Fragment } from 'react';
+import { Button } from './components/Button/Button';
+import { Form } from './components/Form'
 import { useState } from 'react';
-import ListGroup from './components/ListGroup';
 import './App.css';
 import { FaRegGrinTongue } from "react-icons/fa";
 
 function App() {
 
 
-  const [showAlert, setShowAlert] = useState(false);
 
-  const onButtonClick = () => {
-    setShowAlert(true);
-  }
-
-  const handleAlertClose = () => {
-    setShowAlert(false);
-  }
-
-  return (
-    <Fragment>
-      <div>
-        {showAlert ? <Alert onAlertClose={handleAlertClose}>My info alert</Alert> : null}
-      </div>
-      <ListGroup items={["Item 1", "Item 2", "Item 3"]} heading="List of items" onSelectItem={(item) => console.log(item)} />
-      <Button handleClick={onButtonClick} color='success'>Show an alert</Button>
-      <FaRegGrinTongue color="blue" size="40"  />
-      <LikeIcon onClick={() => {console.log("like icon clicked")}} />
-    </Fragment>
-  );
+    return (
+        <>
+            <div>
+                <Form />
+            </div>       
+        </>
+    )
 }
 
 export default App;
---
+++ src/components/Form.tsx
@@ -0,0 +1,35 @@
+import { FormEvent, useRef } from "react"
+import { FieldValues, useForm } from "react-hook-form"
+
+interface FormData {
+    name: string;
+    age: number;
+}
+
+export function Form() {
+    const { register, handleSubmit, formState: {errors}, formState} = useForm<FormData>()
+    console.log(formState.errors)
+
+    const onSubmit = (data: FieldValues) => {
+        console.log(data)
+    }
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="mb-3">
+                <label htmlFor="name" className="form-label">Name</label>
+                <input {...register('name', {required: true, minLength: 4})} id="name" type="text" className="form-control" />
+            </div>
+            {errors.name?.type === 'required' && <p className='text-danger'>The name field is required</p>}
+            {errors.name?.type === 'minLength' && <p className='text-danger'>The name must be at least 4 characters</p>}
+            <div className="mb-3">
+                <label htmlFor="age" className="form-label">Age</label>
+                <input {...register('age')} id="age" type="number" className="form-control" />
+            </div>
+            <button className="btn btn-primary" type="submit">Submit</button>
+        </form>
+    )
+}
+
+
+
