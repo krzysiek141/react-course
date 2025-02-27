30efd8a406c7a0c86b42c1842188a635d472214d
Krzysiek <szlauerdev@gmail.com>
2025-02-27 21:08:07 +0100

    Connecting to backend: 4

--- src/App.tsx
+++ src/App.tsx
@@ -51,19 +51,42 @@ import { ProductList } from "./ProductList";
 // }
 
 
+// function App() {
+
+//   const [category, setCategory] = useState('')
+
+//   return (
+//     <div>
+//       <select className="form-select" onChange={(event) => setCategory(event.target.value)}>
+//         <option value=""></option>
+//         <option value="Clothing">Clothing</option>
+//         <option value="Household">Household</option>
+//       </select>
+//       <ProductList category={category}/>
+//     </div>
+//   )
+// }
+
+// export default App;
+
+
 function App() {
 
-  const [category, setCategory] = useState('')
+  const connect = () => {
+    console.log("connecting")
+  }
+
+  const disconnect = () => {
+    console.log("disconnecting")
+  }
+
+  useEffect(() => {
+    connect()
+    return () => {disconnect()}
+  })
 
   return (
-    <div>
-      <select className="form-select" onChange={(event) => setCategory(event.target.value)}>
-        <option value=""></option>
-        <option value="Clothing">Clothing</option>
-        <option value="Household">Household</option>
-      </select>
-      <ProductList category={category}/>
-    </div>
+    <div></div>
   )
 }
 
